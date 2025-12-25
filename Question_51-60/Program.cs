6cd4b03236a9abd472720aebb93d0fb72b70df41
Emirhan Kaaner <kaaner4mir@gmail.com>
2025-12-25 09:46:45 +0300

    add 10 questions

---
+++ Question_51-60/Program.cs
@@ -0,0 +1,125 @@
+﻿class Program
+{
+    static void Main()
+    {
+        #region 51. 50 den 1′ye azalarak tek sayıları ekrana alt alta yazan programı yapınız.
+
+        //for (int i = 49; i>0; i-=2)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+        #region 52. 1 den 100′ye kadar olana çift sayıları alt alta yazan programı yapınız.
+
+        //for (int i = 2; i <=100; i+=2)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+        #region 53. 0 dan başlayarak klavyeden girilen bir sayıya kadar olan sayıları ekrana alt alta yazan programı yapınız.
+
+        //Console.Write("Bir sayı girin: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = 0; i < number; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+        #region 54. Klavyeden girilen bir sayıdan başlayıp, 1′e kadar olan sayıları alt alta ekrana programı yapınız.
+
+        //Console.Write("Bir sayı girin: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = number; i>1; i--)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+        #region 55. Ekrana 30 defa “Bilişim Teknolojileri” mesajını alt alta yazan programı yapınız.
+
+        //for (int i = 0; i < 30; i++)
+        //{
+        //    Console.WriteLine("Bilişim Teknolojileri");
+        //}
+
+        #endregion
+        #region 56. Klavyeden girilen kullanıcının adını ekrana 20 kez yazan programı yapınız.
+
+        //Console.Write("Bir kullanıcı adı girin: ");
+        //string username = Console.ReadLine();
+
+        //for (int i = 0; i < 20; i++)
+        //{
+        //    Console.WriteLine(username);
+        //}
+
+        #endregion
+        #region 57. Klavyeden girilen iki sayının arasında bulunan tüm sayıları ekrana alt alta yazan programı yapınız.
+
+        //Console.Write("Küçük sayıyı girin: ");
+        //int smallNumber = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("Büyük sayıyı girin: ");
+        //int largeNumber = Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = smallNumber; i < largeNumber; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+        #region 58. Klavyeden girilen 10 sayının toplamını ekrana yazan programı yapınız.
+
+        //int total = 0;
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.Write($"{i+1}. sayıyı girin: ");
+        //    int number = Convert.ToInt32(Console.ReadLine());
+
+        //    total += number;
+        //}
+
+        //Console.WriteLine($"Girilen sayıların toplamı: {total}");
+
+        #endregion
+        #region 59. Klavyeden girilen 10 sayının ortalamasını bulup, ekrana yazan programı yapınız.
+
+        //double total = 0;
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.Write("Bir sayı girin: ");
+        //    int number = Convert.ToInt32(Console.ReadLine());
+
+        //    total += number;
+        //}
+
+        //double average = total / 10;
+
+        //Console.WriteLine($"Sayıların ortalaması: {average}");
+
+        #endregion
+        #region 60. Klavyeden girilen 10 sayıdan sadece negatif olanların çarpımını ekrana programı yapınız.
+
+        //int total = 1;
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.Write($"{i + 1}. sayıyı girin: ");
+        //    int number = Convert.ToInt32(Console.ReadLine());
+
+        //    if (number < 0)
+        //        total *= number;
+        //}
+
+        //Console.WriteLine($"Girilen negatif sayıları çarpımı: {total}");
+
+        #endregion
+    }
+}
